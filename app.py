4145906ce0725eaa5ef617e22d28d8a2335d828e
varshithamurthy21-a11y <varshithamurthy21@gmail.com>
2026-09-17 22:20:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ mode = st.sidebar.selectbox("Navigation Panel", [
 agent = SemanticHelpDeskAgent()
 
 if mode == "Chat UI Interface":
-    st.title("HelpDeskGenie Chat Gateway")
+    st.title(" 🧞 HelpDeskGenie Chat Gateway")
     if "messages" not in st.session_state:
         st.session_state.messages = [{"role": "assistant", "content": "Hello! How can I help you with your IT infrastructure today?"}]
     for msg in st.session_state.messages:
